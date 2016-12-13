c01e1639862b8b77a7efb63a43a4df267d0fad6d
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-13 22:07:17 +0000

    Add failing unit test to test CI

--- src/client/js/index.spec.js
+++ src/client/js/index.spec.js
@@ -3,4 +3,9 @@ describe('Demo', function() {
     // An intentionally failing test. No code within expect() will never equal 4.
     expect(2 + 2).toEqual(4);
   });
+  
+  it('has a dummy spec to test 2 * 3', function() {
+    // An intentionally failing test. No code within expect() will never equal 4.
+    expect(2 * 2).toEqual(6);
+  });
 });
