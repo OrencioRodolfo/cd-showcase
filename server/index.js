10272cea5fc0590cb51468e720c2191e4661cfc2
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-13 21:11:10 +0000

    Add server serving the index

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = 8080
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use('/', express.static(`${process.cwd()}/public`))
+
+app.get('/', (req, res) =>
+  res.sendFile(`${process.cwd()}/public/index.html`)
+)
+
+app.listen(app.get('port'), () => {
+  console.log(`Server listening on port ${app.get('port')}`)
+})
