445c143dda87bc61eef0573aafd9868f5e834f73
orenciorodolfo <orenciorodolfo@hotmail.com>
2016-12-19 23:03:16 +0000

    Add company component for the main screen

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,6 +1,16 @@
 require('./../style/main.pcss');
 import Vue from 'vue';
 
+// Define a new component called todo-item
+Vue.component('company', {
+  template: `
+    <div class="m-img-legend m-company">
+      <img src="img/exictos.png" class="a-img"/>
+      <span class="a-legend a-text-xl">Exictos</span>
+    </div>
+  `
+});
+
 const app = new Vue({
   el: '#app',
   data: {
