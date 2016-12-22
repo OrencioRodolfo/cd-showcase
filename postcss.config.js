d19009e2c1e54ca1bffea4a63aa90c83170a6353
OrencioRodolfo <orenciorodolfo@hotmail.com>
2016-12-22 13:00:19 +0000

    Add postCSS maps and functions

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,21 @@
+const configPath = './src/client/config';
+const theme = 'one'
+const colorPalettes = require(`./${configPath}/color-palettes.js`)(theme)
+const colorFunction = require(`./${configPath}/color-fn.js`)(theme)
+const fontSettings = require(`./${configPath}/font-settings.js`)
+const screenSizes = require(`./${configPath}/screen-sizes.js`)
+
 module.exports = {
   plugins: [
     require('postcss-import')({ /* ...options */ }),
     require('postcss-nested')({ /* ...options */ }),
     require('postcss-simple-vars')({ /* ...options */ }),
-    require('autoprefixer')({ /* ...options */ })
+    require('autoprefixer')({ /* ...options */ }),
+    require('postcss-map')({ maps: [colorPalettes, fontSettings, screenSizes] }),
+    require('postcss-functions')({
+      functions: {
+        'get-color': colorFunction,
+      },
+    }),
   ]
 }
