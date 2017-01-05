785b3f974291ad54996380414159ffad018fac57
OrencioRodolfo <orenciorodolfo@hotmail.com>
2017-01-05 10:35:54 +0000

    Add "divide" operation

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -7,21 +7,41 @@ function getInputValues() {
       first: parseFloat(document.querySelector('#sum-first').value),
       second: parseFloat(document.querySelector('#sum-second').value),
     },
-    decremInputs: {
+    subtractInputs: {
       first: parseFloat(document.querySelector('#subtract-first').value),
       second: parseFloat(document.querySelector('#subtract-second').value),
+    },
+    divideInputs: {
+      first: parseFloat(document.querySelector('#divide-first').value),
+      second: parseFloat(document.querySelector('#divide-second').value),
     }
   };
 }
 
 window.calc = function calc(operation) {
-  const { sumInputs, decremInputs } = getInputValues();
-  if (operation === 'sum') {
-    const result = calculator.sum(sumInputs.first, sumInputs.second);
-    document.querySelector('#sum-result').innerHTML = result;
-  } else if(operation === 'subtract') {
-    const result = calculator.subtract(decremInputs.first, decremInputs.second);
-    document.querySelector('#subtract-result').innerHTML = result;
+  const { sumInputs, subtractInputs, divideInputs } = getInputValues();
+  let result;
+  let element;
+
+  switch(operation) {
+    case 'sum':
+      result = calculator.sum(sumInputs.first, sumInputs.second);
+      element = '#sum-result';
+      document.querySelector('#sum-result').innerHTML = result;
+      break;
+    case 'subtract':
+      result = calculator.subtract(subtractInputs.first, subtractInputs.second);
+      element = '#subtract-result';
+      break;
+    case 'divide':
+      result = calculator.divide(divideInputs.first, divideInputs.second);
+      element = '#divide-result';
+      break;
+    default:
+     throw new Error('Must specify the operation');
+     return;
   }
+
+  document.querySelector(element).innerHTML = result;
 }
 
